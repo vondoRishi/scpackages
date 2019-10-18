48d4396e50a29346d297d559ee970e63987f2253
dasroy <rishi.dasroy@gmail.com>
2019-10-18 19:02:02 +0300

    Report generation

--- R/custom_render.r
+++ R/custom_render.r
@@ -1,5 +1,18 @@
+#' Title
+#'
+#' @param obj_scRNA
+#' @param title
+#'
+#' @return
+#' @export
+#'
+#' @import rmarkdown
+#' @examples
 report_QC <- function(obj_scRNA,title) {
-  library(rmarkdown)
-  render("QC_report.Rmd",output_file = paste(title,format(Sys.time(), '%d_%B_%Y'),sep = "_"), params = list(set_title=title))
+      render(
+        system.file("rmd_template", "QC_report.Rmd", package = "scpackages"),
+        output_file = paste(title, format(Sys.time(), '%d_%B_%Y'), sep = "_"),
+        params = list(set_title = title)
+    )
 }
 
